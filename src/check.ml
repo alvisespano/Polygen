3b6df3eab783caa43465691b6d4dd875231cf82a
Alvise Spanò <spano.alvise@gmail.com>
2018-04-24 16:44:51 +0200

    Progress

--- src/check.ml
+++ src/check.ml
@@ -20,19 +20,19 @@ let unsupported_import () = raise (Unexpected ("import not supported yet"))
 (* LabelSet with loc *)
 
 module LabelLoc : Set.OrderedType with type t = symbol * Err.loc option =
-  struct
+struct
     type t = symbol * Err.loc option
     let compare (lb1, _) (lb2, _) = compare lb1 lb2
-  end
+end
 
 module LabelLocSet =
-  struct
+struct
     include Set.Make (LabelLoc)
 
     let of_labels labels = List.fold_left (fun lbs lb -> add (lb, None) lbs) empty labels
     let occurs lb lbs = mem (lb, None) lbs
     let pretty lbs = "{ " ^ (flatten_strings ", " (List.map fst (elements lbs))) ^ " }"
-  end
+end
 
 
 (* label group and group set *)
@@ -40,7 +40,7 @@ module LabelLocSet =
 type group = Closed of LabelSet.t | Open of LabelSet.t
 
 module Group =
-  struct
+struct
     type t = group
 
     let pretty = function
@@ -52,7 +52,7 @@ module Group =
         | (Open lbs1, Open lbs2)
         | (Closed lbs1, Closed lbs2) -> LabelSet.compare lbs1 lbs2
         | _                          -> 1
-  end
+end
 
 module GroupSet = Set.Make (Group)
 
@@ -68,10 +68,10 @@ let fresh_uid_of_symbol sym = Path.of_sym sym, (incr counter; !counter)
 (* branch and circuits *)
 
 module Branch =
-  struct
+struct
     type t = uid * LabelLocSet.t
     let (=) (uid, lbs) (uid', lbs') = uid = uid' && LabelLocSet.equal lbs lbs'
-  end
+end
 
 module BranchCache = Prelude.Cache (Branch)
 
@@ -85,53 +85,53 @@ let pretty_branches bs =
 (* termination magic ring *)
 
 type termination =
-    Zero
-  | Epsilon of Err.loc
-  | WeakEpsilon of Err.loc
-  | Term
-  | Unterm of Err.loc * Branch.t list
+      Zero
+    | Epsilon of Err.loc
+    | WeakEpsilon of Err.loc
+    | Term
+    | Unterm of Err.loc * Branch.t list
 
 let reduce_seq = function
-    (Zero, t)
-  | (t, Zero)                           -> t
+      (Zero, t)
+    | (t, Zero)                           -> t
 
-  | (Unterm (loc, bs), Unterm _)        -> Unterm (loc, bs)
+    | (Unterm (loc, bs), Unterm _)        -> Unterm (loc, bs)
 
-  | (Epsilon loc, Epsilon _)            -> Epsilon loc
+    | (Epsilon loc, Epsilon _)            -> Epsilon loc
 
-  | (WeakEpsilon loc, WeakEpsilon _)
+    | (WeakEpsilon loc, WeakEpsilon _)
 
-  | (Epsilon _, WeakEpsilon loc)
-  | (WeakEpsilon loc, Epsilon _)        -> WeakEpsilon loc
+    | (Epsilon _, WeakEpsilon loc)
+    | (WeakEpsilon loc, Epsilon _)        -> WeakEpsilon loc
 
-  | (Unterm (loc, bs), _)
-  | (_, Unterm (loc, bs))               -> Unterm (loc, bs)
+    | (Unterm (loc, bs), _)
+    | (_, Unterm (loc, bs))               -> Unterm (loc, bs)
 
-  | (Term, _)
-  | (_, Term)                           -> Term
+    | (Term, _)
+    | (_, Term)                           -> Term
 
 let reduce_prod = function
-    (Zero, t)
-  | (t, Zero)                           -> t
+      (Zero, t)
+    | (t, Zero)                           -> t
 
-  | (Unterm (loc, bs), Unterm _)        -> Unterm (loc, bs)
+    | (Unterm (loc, bs), Unterm _)        -> Unterm (loc, bs)
 
-  | (Epsilon loc, Epsilon _)            -> Epsilon loc
+    | (Epsilon loc, Epsilon _)            -> Epsilon loc
 
-  | (WeakEpsilon loc, WeakEpsilon _)    -> WeakEpsilon loc
+    | (WeakEpsilon loc, WeakEpsilon _)    -> WeakEpsilon loc
 
-  | (Epsilon _, WeakEpsilon loc)
-  | (WeakEpsilon loc, Epsilon _)        -> WeakEpsilon loc
+    | (Epsilon _, WeakEpsilon loc)
+    | (WeakEpsilon loc, Epsilon _)        -> WeakEpsilon loc
 
-  | (Term, Term)                        -> Term
+    | (Term, Term)                        -> Term
 
-  | (Unterm _, t)
-  | (t, Unterm _)                       -> t
+    | (Unterm _, t)
+    | (t, Unterm _)                       -> t
 
-  | (Term, Epsilon loc)
-  | (Term, WeakEpsilon loc)
-  | (Epsilon loc, Term)
-  | (WeakEpsilon loc, Term)             -> WeakEpsilon loc
+    | (Term, Epsilon loc)
+    | (Term, WeakEpsilon loc)
+    | (Epsilon loc, Term)
+    | (WeakEpsilon loc, Term)             -> WeakEpsilon loc
 
 
 (* side checkers *)
@@ -157,8 +157,8 @@ let check_useless_permutations atoms =
     let f z (a, loc) =
         let rec recur = function
             | ( A.Fold (A.Sub (A.Mob, _, _))
-            | A.Unfold (A.Sub (A.Mob, _, _))
-            | A.Lock (A.Sub (A.Mob, _, _))) -> true
+              | A.Unfold (A.Sub (A.Mob, _, _))
+              | A.Lock (A.Sub (A.Mob, _, _))) -> true
             | A.Sel ((a', _), _)            -> recur a'
             | _                             -> false
         in
@@ -228,7 +228,7 @@ let check_basic =
         | A.Fold (A.NonTerm path)
         | A.Unfold (A.NonTerm path) ->
             (try ignore (Env.lookup env path)
-            with Not_found -> error loc (sprintf "undefined non-terminal symbol '%s'" (Path.pretty path)))
+             with Not_found -> error loc (sprintf "undefined non-terminal symbol '%s'" (Path.pretty path)))
 
         | A.Lock (A.Sub (_, decls, p))
         | A.Fold (A.Sub (_, decls, p))
@@ -238,12 +238,12 @@ let check_basic =
             in
                 check_prod env' prefixes p
 
-    in
-        fun decls ->
-            let prefixes = [] in
-            let env0 = declare Env.empty prefixes decls
-            in
-                check_decls env0 prefixes decls
+    in fun decls ->
+
+        let prefixes = [] in
+        let env0 = declare Env.empty prefixes decls
+        in
+            check_decls env0 prefixes decls
 
 
 (* unfolding checker:
@@ -252,6 +252,7 @@ let check_basic =
 *)
 
 type envv = { uid : uid; mode : A.bind_mode; prod : A.prod }
+
 type env = envv Env.t
 
 let check_unfolding =
@@ -285,28 +286,28 @@ let check_unfolding =
 
     and check_atom env uids (a, loc) =
         let _ = check_useless_unfoldings (a, loc) in
-        match a with
-        | A.Terminal _ -> ()
+            match a with
+            | A.Terminal _ -> ()
 
-        | A.Sel (a, _) -> check_atom env uids a
+            | A.Sel (a, _) -> check_atom env uids a
 
-        | A.Lock (A.NonTerm _)
-        | A.Fold (A.NonTerm _) -> ()
+            | A.Lock (A.NonTerm _)
+            | A.Fold (A.NonTerm _) -> ()
 
-        | A.Unfold (A.NonTerm path) ->
-            let { uid = uid; mode = m; prod = p } = Env.lookup env path in
-            (match m with
-            | A.Assign -> warning 2 loc (sprintf "unfolding of assignment-bound symbol '%s'" (Path.pretty path))
-            | _        -> ());
-            if occurs uids uid then error loc (sprintf "cyclic unfolding of symbol '%s'" (Path.pretty path))
-            else check_prod env (uid :: uids) p
+            | A.Unfold (A.NonTerm path) ->
+                let { uid = uid; mode = m; prod = p } = Env.lookup env path in
+                    (match m with
+                     | A.Assign -> warning 2 loc (sprintf "unfolding of assignment-bound symbol '%s'" (Path.pretty path))
+                     | _        -> ());
+                    if occurs uids uid then error loc (sprintf "cyclic unfolding of symbol '%s'" (Path.pretty path))
+                    else check_prod env (uid :: uids) p
 
-        | A.Lock (A.Sub (_, decls, p))
-        | A.Fold (A.Sub (_, decls, p))
-        | A.Unfold (A.Sub (_, decls, p)) ->
-            let env' = declare env decls in
-            check_decls env' uids decls;
-            check_prod env' uids p
+            | A.Lock (A.Sub (_, decls, p))
+            | A.Fold (A.Sub (_, decls, p))
+            | A.Unfold (A.Sub (_, decls, p)) ->
+                let env' = declare env decls in
+                    check_decls env' uids decls;
+                    check_prod env' uids p
 
     in
         fun decls -> check_decls (declare Env.empty decls) [] decls
@@ -330,9 +331,9 @@ let report_groups (sym, loc) gs =
         in
         let prefix = sym ^ " :: " in
         let tab = String.make (String.length prefix) ' ' in
-        match gs with
-        | []      -> info loc (prefix ^ "?{}")
-        | g :: gs -> minfo loc (prefix ^ (Group.pretty g)) (List.map (fun g -> tab ^ (Group.pretty g)) gs)
+            match gs with
+            | []      -> info loc (prefix ^ "?{}")
+            | g :: gs -> minfo loc (prefix ^ (Group.pretty g)) (List.map (fun g -> tab ^ (Group.pretty g)) gs)
     else ()
 
 let report_termination sym t =
@@ -375,8 +376,8 @@ let check_termination lbs decls start =
     and check_global_decls args decls =
         let report sym loc =
             let gs, t = check_bind args (sym, loc) in
-            report_termination sym t;
-            report_groups (sym, loc) gs
+                report_termination sym t;
+                report_groups (sym, loc) gs
         in
         let f r (decl, loc) =
             match decl with
@@ -391,34 +392,34 @@ let check_termination lbs decls start =
         match a with
         | A.Terminal t ->
             (match t with
-            | A.Epsilon
-            | A.Capitalize
-            | A.Concat     -> GroupSet.empty, Epsilon loc
-            | A.Term sym   -> GroupSet.empty, Term)
+             | A.Epsilon
+             | A.Capitalize
+             | A.Concat     -> GroupSet.empty, Epsilon loc
+             | A.Term sym   -> GroupSet.empty, Term)
 
-    | A.Sel (a', None)    -> check_atom { args with lbs = LabelLocSet.empty } a'
-    | A.Sel (a', Some lb) ->
-        let gs, t = check_atom { args with lbs = LabelLocSet.add (lb, Some loc) args.lbs } a' in
-        let gs' = GroupSet.filter (fun (Closed lbs | Open lbs) -> not (LabelSet.occurs lb lbs)) gs
-        in
-            gs', t
+        | A.Sel (a', None)    -> check_atom { args with lbs = LabelLocSet.empty } a'
+        | A.Sel (a', Some lb) ->
+            let gs, t = check_atom { args with lbs = LabelLocSet.add (lb, Some loc) args.lbs } a' in
+            let gs' = GroupSet.filter (fun (Closed lbs | Open lbs) -> not (LabelSet.occurs lb lbs)) gs
+            in
+                gs', t
 
-    | A.Lock (A.NonTerm path)
-    | A.Fold (A.NonTerm path)
-    | A.Unfold (A.NonTerm path) -> branch (args, loc, path)
+        | A.Lock (A.NonTerm path)
+        | A.Fold (A.NonTerm path)
+        | A.Unfold (A.NonTerm path) -> branch (args, loc, path)
 
-    | A.Lock (A.Sub (_, decls, p))
-    | A.Fold (A.Sub (_, decls, p))
-    | A.Unfold (A.Sub (_, decls, p)) ->
-        let r' = { args with env = declare args.env decls } in
-        let _ = check_nested_decls r' decls
-        in
-            check_prod r' p
+        | A.Lock (A.Sub (_, decls, p))
+        | A.Fold (A.Sub (_, decls, p))
+        | A.Unfold (A.Sub (_, decls, p)) ->
+            let r' = { args with env = declare args.env decls } in
+            let _ = check_nested_decls r' decls
+            in
+                check_prod r' p
 
 
     and check_seq args (A.Seq (_, atoms), _) =
         let f (gs, t) a =
-            let gs', t') = check_atom args a
+            let gs', t' = check_atom args a
             in
                 (GroupSet.union gs gs', reduce_seq (t, t'))
         in
@@ -428,41 +429,41 @@ let check_termination lbs decls start =
     and check_prod args (A.Prod seqs, loc) =
         let g =
             let lbs =
-                    let f z = function A.Seq (None, _), _    -> z
-                                     | A.Seq (Some lb, _), _ -> LabelSet.add lb z
-                    in
-                        List.fold_left f LabelSet.empty seqs
-            in
-                (if (List.exists (function A.Seq (None, _), _ -> true | _ -> false) seqs) then Open else Closed) lbs
-        in
-            let gs, t =
-                let seqs' =
-                    if LabelLocSet.is_empty args.lbs then seqs
-                    else
-                        let f (A.Seq (lbo, _), _) =
-                            match lbo with
-                            | None    -> true
-                            | Some lb -> LabelLocSet.occurs lb args.lbs
-                        in
-                            List.filter f seqs
+                let f z = function A.Seq (None, _), _    -> z
+                                 | A.Seq (Some lb, _), _ -> LabelSet.add lb z
                 in
-                    if List.length seqs' = 0 then
-                        let ss =
-                            let f (lb, loco) = "'" ^ lb ^ "' activated " ^ (match loco with Some loc -> localized_msg loc | None -> "at top level")
-                            in
-                                List.map f (LabelLocSet.elements args.lbs)
-                        in
-                            mwarning 1 loc "destructive selection" ("due to current label environment:" :: ss);
-                            GroupSet.empty, Epsilon loc
-                    else
-                        let f (gs, t) seq =
-                            let gs', t' = check_seq args seq
-                            in
-                                GroupSet.union gs gs', reduce_prod (t, t')
-                        in
-                            List.fold_left f (GroupSet.empty, Zero) seqs'
+                    List.fold_left f LabelSet.empty seqs
+            in
+                if (List.exists (function A.Seq (None, _), _ -> true | _ -> false) seqs) then Open lbs else Closed lbs
+        in
+        let gs, t =
+            let seqs' =
+                if LabelLocSet.is_empty args.lbs then seqs
+                else
+                    let f (A.Seq (lbo, _), _) =
+                        match lbo with
+                        | None    -> true
+                        | Some lb -> LabelLocSet.occurs lb args.lbs
                     in
-                        GroupSet.add g gs, t
+                        List.filter f seqs
+            in
+                if List.length seqs' = 0 then
+                    let ss =
+                        let f (lb, loco) = "'" ^ lb ^ "' activated " ^ (match loco with Some loc -> localized_msg loc | None -> "at top level")
+                        in
+                            List.map f (LabelLocSet.elements args.lbs)
+                    in
+                        mwarning 1 loc "destructive selection" ("due to current label environment:" :: ss);
+                        GroupSet.empty, Epsilon loc
+                else
+                    let f (gs, t) seq =
+                        let gs', t' = check_seq args seq
+                        in
+                            GroupSet.union gs gs', reduce_prod (t, t')
+                    in
+                        List.fold_left f (GroupSet.empty, Zero) seqs'
+        in
+            GroupSet.add g gs, t
 
 
     and branch (args, loc, path) =
@@ -474,25 +475,23 @@ let check_termination lbs decls start =
         match tail_segment (Branch.(=) (uid, args.lbs)) args.branches with
         | []       -> check_prod {args with branches = (uid, args.lbs) :: args.branches} p
         | branches -> (GroupSet.empty, Unterm (loc, branches))
-
-  
     in
-        let r = { lbs = LabelLocSet.of_labels (LabelSet.elements lbs);
-                  branches = [];
-                  env = declare Env.empty decls }
-        in
-            check_global_decls r decls
+    let r = { lbs = LabelLocSet.of_labels (LabelSet.elements lbs);
+              branches = [];
+              env = declare Env.empty decls }
+    in
+        check_global_decls r decls
 
 
 (* whole checker *)
 
-let check lbs decls start =
+let check_all lbs decls start =
     let check_start_nonterm sym what descr =
         try ignore (List.find (function (A.Bind (_, sym', _), _) -> sym = sym' | _ -> false) decls)
         with Not_found -> uwarning 1 (sprintf "undefined %s symbol %s: %s" what sym descr)
     in
-    check_start_nonterm start "start" "grammar does not have a default entry point";
-    check_start_nonterm "I" "info" "option -info will not work";
-    check_basic decls;
-    check_unfolding decls;
-    (*check_termination lbs decls start;*)
+        check_start_nonterm start "start" "grammar does not have a default entry point";
+        check_start_nonterm "I" "info" "option -info will not work";
+        check_basic decls;
+        check_unfolding decls;
+        check_termination lbs decls start;
