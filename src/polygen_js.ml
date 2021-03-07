1f990efdb2266888282e5f76aa5500c514b463db
Paolo Donadeo <p.donadeo@gmail.com>
2021-03-07 12:27:58 +0100

    JS: configuration for generate function completed

--- src/polygen_js.ml
+++ src/polygen_js.ml
@@ -3,33 +3,42 @@ open Js
 
 
 class type config = object
-  method start : js_string Js.t optdef readonly_prop
+  method start : js_string t optdef readonly_prop
+  method lbs : js_string t js_array t optdef readonly_prop
 end
 
 let _ =
-  Js.export "Polygen"
+  export "Polygen"
     (object%js
-       method generate grammar (config : config Js.t optdef) =
+       method generate grammar (config : config t optdef) =
         let config =
           match (Optdef.to_option config) with
           | Some config -> config
           | None -> begin
               object%js
-                val start = Optdef.return (Js.string "S")
+                val start = Optdef.return (string "S")
+                val lbs = Optdef.return (array [||])
               end
           end in
 
-        let grammar = Js.to_string grammar in
-        let start_js = Optdef.get (config##.start) (fun () -> Js.string "S") in
-        let start = Js.to_string start_js in
+        let grammar = to_string grammar in
+        let start = Optdef.get (config##.start) (fun () -> string "S") |> to_string in
+        let lbs =
+          Optdef.get (config##.lbs) (fun () -> array [||])
+          |> to_array
+          |> Array.to_list
+          |> List.map to_string
+          |> Polygen_lib.Prelude.LabelSet.of_labels in
 
         let decls = Polygen_lib.load_decls grammar in
         match decls with
         | Ok decls -> begin
-          let output = Polygen_lib.generate ~start decls in
-          Js.string output
+          let output = Polygen_lib.generate ~lbs ~start decls in
+          string output
         end
         | Error e -> begin
-          Js.string e
+          string e
         end
+      method pRngInit (seed : int) : int =
+        Polygen_lib.pRNG_init ~seed:(Some seed) ()
      end)
