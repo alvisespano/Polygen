cc72b1a995b0f19d038be84fe45622d92ffeeb49
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-11-26 05:20:31 +0100

    Add files via upload

--- mkmusic.sh
+++ mkmusic.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 while [ true ]; do
-	polygen /usr/share/polygen/music.grm | abc2midi - -o /tmp/xy.mid
-	wildmidi /tmp/xy.mid
+	rm /tmp/xy*.mid
+	polygen /usr/share/polygen/music.grm > /tmp/xy.abc
+       	abc2midi /tmp/xy.abc -Q 90
+	echo dir /usr/share/midi/freepats/Tone_000/ > /tmp/wildmidi.cfg 
+	echo bank 0 >> /tmp/wildmidi.cfg 
+	ls /usr/share/midi/freepats/Tone_000/ | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
+	cat /tmp/xy*.mid > /tmp/abc.mid
+	wildmidi -c /tmp/wildmidi.cfg /tmp/abc.mid
+	#wildmidi /tmp/xy.mid
 	wait.exe
 done
 
