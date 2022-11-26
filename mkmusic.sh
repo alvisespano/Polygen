d6691b74a55406be6676f33a532f287998e6ce98
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-11-26 05:54:12 +0100

    Add files via upload

--- mkmusic.sh
+++ mkmusic.sh
@@ -2,14 +2,23 @@
 
 while [ true ]; do
 	rm /tmp/xy*.mid
+	rm /tmp/xy*.wav
 	polygen /usr/share/polygen/music.grm > /tmp/xy.abc
        	abc2midi /tmp/xy.abc -Q 90
-	echo dir /usr/share/midi/freepats/Tone_000/ > /tmp/wildmidi.cfg 
-	echo bank 0 >> /tmp/wildmidi.cfg 
-	ls /usr/share/midi/freepats/Tone_000/ | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
-	cat /tmp/xy*.mid > /tmp/abc.mid
-	wildmidi -c /tmp/wildmidi.cfg /tmp/abc.mid
-	#wildmidi /tmp/xy.mid
+
+	for x in /tmp/xy*.mid; do
+		echo dir /usr/share/midi/freepats/Tone_000/ > /tmp/wildmidi.cfg 
+		echo bank 0 >> /tmp/wildmidi.cfg 
+		ls /usr/share/midi/freepats/Tone_000/ | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
+		wildmidi -c /tmp/wildmidi.cfg -o $x.wav $x
+	done
+
+	inp="`find /tmp -maxdepth 1 -iname "xy*wav" -printf " -i \"%h/%f\" "`"
+
+	ffmpeg -y $inp -filter_complex:a amerge /tmp/xy.mp3
+
+	mpg123 /tmp/xy.mp3
+
 	wait.exe
 done
 
