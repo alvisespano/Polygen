af7cca2c0da5386da33ade88f5de6092cf7461b3
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-11-26 11:55:16 +0100

    Add files via upload

--- polygen_examples/mkmusic.sh
+++ polygen_examples/mkmusic.sh
@@ -4,10 +4,10 @@ doit() {
 	cp ./music.grm.tmpl /tmp
 	echo $1 ";" >> /tmp/music.grm.tmpl
 	polygen /tmp/music.grm.tmpl > /tmp/xy.abc
-       	abc2midi /tmp/xy.abc -o /tmp/xy.wav -Q 90
+       	abc2midi /tmp/xy.abc -o /tmp/xy.mid -Q 90
 	echo $3 > /tmp/wildmidi.cfg 
 	ls $2/*pat | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
-	wildmidi -c /tmp/wildmidi.cfg -o `mktemp`.xy.wav /tmp/xy.wav
+	wildmidi -c /tmp/wildmidi.cfg -o `mktemp`.xy.wav /tmp/xy.mid
 }
 
 while [ true ]; do
@@ -16,16 +16,16 @@ while [ true ]; do
 	rm /tmp/*xy*.mp3
 	rm /tmp/song.mp3
 
-	doit "Note ::= (\"c\"|\"d\"|\"e\"|\"f\"|\"g\"|\"a\"|\"b\") ^(\"/\"|\"2\"|\"\")" "/opt/polgen_examples/Tone_001" "bank 0"
+	doit "Note ::= (\"c\"|\"d\"|\"e\"|\"f\"|\"g\"|\"a\"|\"b\") ^(\"/\"|\"2\"|\"\")" "/opt/polygen_examples/Tone_001" "bank 0"
 	doit "Note ::= (\"C\"|\"D\"|\"E\"|\"F\"|\"G\"|\"A\"|\"B\") ^(\"2\"|\"3\"|\"4\")" "/opt/polygen_examples/Tone_001" "bank 0"
 	doit "Note ::= (\"C\"|\"D\"|\"E\"|\"F\"|\"G\"|\"A\"|\"B\") ^(\"2\"|\"3\"|\"4\")" "/opt/polygen_examples/Drum_001" "drumbank 0"
 	doit "Note ::= (\"C\"|\"D\"|\"E\"|\"F\"|\"G\"|\"A\"|\"B\") ^(\"2\"|\"3\"|\"4\")" "/opt/polygen_examples/Drum_001" "drumbank 0"
 
-	for x in /tmp/*xy*wav;do
+	for x in /tmp/*xy.wav;do
 		ffmpeg -i $x -filter:a loudnorm $x.mp3
 	done
 
-	inp="`find /tmp -maxdepth 1 -iname "xy*.mp3" -printf " -stream_loop -1 -t 180s -i \"%h/%f\" "`"
+	inp="`find /tmp -maxdepth 1 -iname "*xy*.mp3" -printf " -stream_loop -1 -t 180s -i \"%h/%f\" "`"
 
 	ffmpeg $inp -filter_complex:a amerge=inputs=`ls /tmp/*xy*.mp3 | wc -l` /tmp/song.mp3
 
