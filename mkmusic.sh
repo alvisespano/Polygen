acd183ea502d56a1c5db3706012bed72b76e0f47
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-11-26 06:49:52 +0100

    Add files via upload

--- mkmusic.sh
+++ mkmusic.sh
@@ -4,13 +4,14 @@ while [ true ]; do
 	rm /tmp/xy*.mid
 	rm /tmp/xy*.wav
 	rm /tmp/xy*.mp3
+	rm /tmp/song.mp3
 
 	polygen /usr/share/polygen/music.grm > /tmp/xy.abc
        	abc2midi /tmp/xy.abc -Q 90
 	for x in /tmp/xy*.mid; do
 		echo dir /usr/share/midi/freepats/Tone_000/ > /tmp/wildmidi.cfg 
 		echo bank 0 >> /tmp/wildmidi.cfg 
-		ls /usr/share/midi/freepats/Tone_000/ | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
+		ls /usr/share/midi/freepats/Tone_000/*pat | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
 		wildmidi -c /tmp/wildmidi.cfg -o $x.wav $x
 	done
 
@@ -19,7 +20,7 @@ while [ true ]; do
 	for x in /tmp/xyd*.mid; do
 		echo dir /usr/share/midi/freepats/Drum_000/ > /tmp/wildmidi.cfg 
 		echo drumbank 0 >> /tmp/wildmidi.cfg 
-		ls /usr/share/midi/freepats/Drum_000/ | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
+		ls /usr/share/midi/freepats/Drum_000/*pat | shuf | nl -v 0 >> /tmp/wildmidi.cfg 
 		wildmidi -c /tmp/wildmidi.cfg -o $x.wav $x
 	done
 
@@ -27,12 +28,14 @@ while [ true ]; do
 		ffmpeg -i $x -filter:a loudnorm $x.mp3
 	done
 
-	inp="`find /tmp -maxdepth 1 -iname "xy*.mp3" -printf " -stream_loop -1 -t 120s -i \"%h/%f\" "`"
+	inp="`find /tmp -maxdepth 1 -iname "xy*.mp3" -printf " -stream_loop -1 -t 180s -i \"%h/%f\" "`"
 
-	ffmpeg -y $inp -filter_complex:a amerge=inputs=`ls /tmp/xy*.mp3 | wc -l` /tmp/song.mp3
+	ffmpeg $inp -filter_complex:a amerge=inputs=`ls /tmp/xy*.mp3 | wc -l` /tmp/song.mp3
+
+	cp /tmp/song.mp3 ~/$RANDOM.mp3
 
 	mpg123 /tmp/song.mp3
 
-	wait.exe
+	#wait.exe
 done
 
